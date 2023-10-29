6c54598069d61d0820f2824f4482999da8e40199
Sergey Lysyuk <lopdog01@yandex.ru>
2023-10-29 14:58:37 +0300

    Refactor

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,5 +5,16 @@ terraform {
       version = ">= 0.85"
       source  = "yandex-cloud/yandex"
     }
-  }  
+  }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-state-momo"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+  
 }
